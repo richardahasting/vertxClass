76387fc1a12a43ad42fb4f2a67a170444abce855
Richard Hasting <richardahasting@gmail.com>
2021-06-22 18:13:07 -0500

    log4j additions

---
+++ src/main/java/com/richardahasting/verticles/VerticleN.java
@@ -0,0 +1,25 @@
+package com.richardahasting.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+public class VerticleN extends AbstractVerticle {
+
+  final static Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    LOG.debug("Start: " + getClass().getName() + "\t" + Thread.currentThread().getName()
+      +
+      " with config " + config().toString());
+    startPromise.complete();
+  }
+  @Override
+  public void stop(final Promise<Void> stopPromise)
+  {
+    LOG.debug("Stop: " + getClass().getName() + "\t" + Thread.currentThread().getName() +
+      " with config " + config().toString());
+    stopPromise.complete();
+  }
+}
